f759811edcd8f4eea98454738abfba5c4c964711
Gabriele Dias <gabrielemartinsdias13@gmail.com>
2026-08-09 16:47:25 -0300

    test: cobre login e redirecionamento do dashboard

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreAccessTests(TestCase):
+    def test_login_page_is_rendered(self):
+        response = self.client.get(reverse('core:login'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Entrar')
+        self.assertContains(response, 'form method="post" class="login-form"')
+        self.assertContains(response, 'name="username"')
+        self.assertContains(response, 'name="password"')
+
+    def test_root_requires_login(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/login/?next=/')
