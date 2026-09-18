2353734a7db486a0d44dd7ede9a2f194fc754fad
Gabriele Dias <gabrielemartinsdias13@gmail.com>
2026-09-17 22:06:47 -0300

    Adotando versão da colega

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Produto
-
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco')
-    search_fields = ('nome',)
-=======
 
 # Register your models here.
->>>>>>> Parte_Front/main
