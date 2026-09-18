a7a69bd6cf132eda60288abecc14a748314634c8
Gabriele Dias <gabrielemartinsdias13@gmail.com>
2026-09-17 22:16:47 -0300

    FEAT: add settings

--- setup/settings.py
+++ setup/settings.py
@@ -59,10 +59,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'setup.urls'
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "frontend" / "templates"],  # adiciona os templates dela
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,6 +78,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'setup.wsgi.application'
 
